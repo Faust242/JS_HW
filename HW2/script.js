83e09489bc18a11c865409c24ee83305e0d1fd9f
Faust242 <-add>
2022-10-04 01:10:31 +0300

    Made HW2 in  script.js

---
+++ HW2/script.js
@@ -0,0 +1,25 @@
+'use strict'
+
+// First task
+let userAge = 30;
+const userName = 'John';
+const userSurname = 'Malkovich';
+
+// Second task
+
+const newUser = 'Albert Einshtein';
+let listPurchases  = ['car', 'house', 'airplane'];
+let online = true;
+let workerWage = 3000;
+let mouseCoordinate = [0, 0];
+
+// Third task
+
+console.log('Hello, World');
+
+console.log(`userName: ${userName}`);
+console.log(`userAge: ${userAge} years old`);
+
+
+
+
