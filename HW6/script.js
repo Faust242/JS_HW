db4686bd08c130377dbbb97914808b891c77f05f
Faust242 <-add>
2022-10-12 13:33:15 +0300

    HW6--DONE

--- HW6/script.js
+++ HW6/script.js
@@ -1,30 +1,42 @@
-function validationName(){
-    let bool = true;
-    
+// First task
+
+function nameValidation(question, wrongAnswer){
+    let bool =true;
+    let normalName;
     do{
-    let nameValue = prompt('What is your name?');
-    if (nameValue === null || nameValue=='' || nameValue !== ' '){
-        alert('Enter your name, please')
-        continue;
-    } else if (typeof nameValue === 'string'){
-        alert(nameValue);
-        console.log(nameValue)
-        bool = false;  
-    }
-    let surnameValue = prompt('What is your surname?');
-    if (surnameValue === null || surnameValue=='' || surnameValue !== ' '){
-        alert('Enter your surname, please')
-        continue;
-    } else if (typeof  surnameValue === 'string'){
-        alert(surnameValue);
-        console.log(surnameValue)
-        bool = false;  
-    }
-
-
-    let password= prompt ("Enter password")
+    let userName = prompt(question);
+    if (userName === null || userName =='' || userName == ' '){
+        alert(wrongAnswer)
+    } else if (typeof userName === 'string'){
+        firstLetter = userName.slice(0,1);
+        normalName = firstLetter.toUpperCase() + userName.toLowerCase().slice(1,userName.length);
+        return normalName;
+    } 
+    } while (bool)
+}
+function passValidation (){
+    let  bool = true;
+    do{
+        let userPassword= prompt ("Enter password")
+        let lowCasePassword = userPassword.toLowerCase();
+        if ((userPassword.length<6 || userPassword === lowCasePassword) && (userPassword !== null)){
+            alert('Password have to be more than 6 characters, and include upper case and lower case. TRY AGAIN!!!');
+        } else return userPassword;
     } while (bool);
-
 }
 
-validationName();   
+function loginValidation(){
+        let userName = nameValidation('What is your name?', 'Enter your name, please');
+        let userSurname = nameValidation('What is your surname?', 'Enter your surname, please');
+        let userPassword = passValidation();
+        alert(`${userName} ${userSurname}, Welcome to our website, your password: ${userPassword}`)
+}
+
+
+loginValidation();   
+
+// Second task
+randomNum = (a,b)  =>  c=Math.floor(Math.random()*(b-a+1))+a;
+
+console.log(randomNum(3,9));
+console.log(randomNum(5,10));
