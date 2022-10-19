bf9dddb07de2ea0670e488f44cc0359d8f121ace
Faust242 <-add>
2022-10-19 22:45:09 +0300

    HW8--DONE

---
+++ HW8/script.js
@@ -0,0 +1,98 @@
+
+
+// First task
+
+const countTrue = (booleanArr) => {
+    counter = 0;
+    booleanArr.forEach((item, i) =>{
+       if (item === true){
+        counter++;
+       }
+    })
+    return counter
+}
+
+console.log(countTrue([true, false, false, true, false])); // -> 2
+console.log(countTrue([false, false, false, false])); // -> 0
+console.log(countTrue([])); // -> 0
+
+
+
+// Second task
+
+const getOccurrencesCount = (arr) => {
+    const newObj = {};       
+    for (let i in arr){     
+        newObj[arr[i]] = 0
+        for (let j in arr){
+            if (arr[i]===arr[j]){
+            newObj[arr[i]] += +1;
+        } 
+        }
+    }
+    return newObj;
+}
+
+console.log(getOccurrencesCount(["a", "v", "a", "b", "b"]));
+// {
+//   a: 2,
+//   v: 1,
+//   b: 2,
+// }
+
+console.log(getOccurrencesCount([
+  "apples",
+  "oranges",
+  "pears",
+  "pears",
+  "apples",
+  "oranges",
+  "oranges",
+  "pears",
+]));
+// {
+//   apples: 2,
+//   oranges: 3,
+//   pears: 3,
+// }
+
+
+//Third task
+
+const findExcess= (arr)=>{
+    const checkArr = [];
+    let countOdd = 0;
+    let countEven = 0;
+    let extraNumber;
+    arr.forEach(function(item, i){
+        item += 2%2 
+// Check even or odd
+        checkArr[i] = item%2
+// Count even and odd in array
+        if ( checkArr[i] ===0 ){
+            countEven++;
+        } else countOdd++
+// Search extraNumber
+        if (countEven<countOdd){
+            for (let i in arr){
+               if ((arr[i]+2)%2 === 0){
+                extraNumber = arr[i]
+               }
+            }
+        } else {for (let i in arr){
+            if ((arr[i]+2)%2 !== 0){
+             extraNumber = arr[i]
+            }
+         }}
+    }
+    )  
+    return extraNumber
+}
+
+console.log(findExcess([0, 1, 2])); // -> 1
+console.log(findExcess([1, 2, 3])); // -> 2
+console.log(findExcess([2, 6, 8, 10, 3])); // -> 3
+console.log(findExcess([0, 0, 3, 0, 0])); // -> 3
+console.log(findExcess([1, 1, 0, 1, 1])); // -> 0
+
+
