8bd1c5a50d67eb00c63d39dc568dbc532e267406
Faust242 <-add>
2022-10-28 02:02:40 +0300

    HW9+New+Fixed all bugs

--- HW9/script.js
+++ HW9/script.js
@@ -2,43 +2,133 @@
 
 // 1.1
 // С помощью метода массива map преобразуйте массив чисел в массив булевых значений.
-
 // Принцип преобразования следующий: если число больше 0, в итоговый массив записывается true, если меньше - false.
-
-// [1, 2, -10, -2, 7]; // -> [true, true, false, false, true]
-
 function changingArr (arr){
-  let newArr =[];
   arr.map(function(item, i){
     if (item >= 0) {
-        newArr[i] = true
+        arr[i] = true
       } else if (item < 0 ){
-        newArr[i] = false
+        arr[i] = false
       }
    })
-   return newArr
+   return arr
 }
-console.log(changingArr([1, 2, -10, -2, 7]))
+console.log(changingArr([1, 2, -10, -2, 7])) // [1, 2, -10, -2, 7]; // -> [true, true, false, false, true]
 
 
 // 1.2
 // Дан массив пользователей (в примере ниже)
-
 // Напишите функцию, которая с помощью комбинации методов массива filter и map будет возвращать массив имен взролых (старше 18 лет) администраторов (role === 'admin').
+const users = [
+  {
+    name: 'Sam',
+    role: 'admin',
+    age: 25,
+  },
+    {
+    name: 'Peter',
+    role: 'admin',
+    age: 16,
+  },
+    {
+    name: 'Pablo',
+    role: 'admin',
+    age: 32,
+  },
+    {
+    name: 'Enrico',
+    role: 'client',
+    age: 43,
+  },
+    {
+    name: 'Mary',
+    role: 'client',
+    age: 34,
+  },
+    {
+    name: 'Katerine',
+    role: 'guest',
+    age: 17,
+  },
+]
+const adultAdmins = (arr) =>{
+  let newArr= []
+  arr.filter(function(item, i) {
+    if (item.age >=18 && item.role === 'admin'){
+      newArr.push(item.name)
+    }
+  })
+  return newArr
+}
+console.log(adultAdmins(users))  // [Sam, Pablo]
 
 
+
+// 1.3
+// Дан массив чисел
+// С помощью метода reduce вычислите среднее арифметическое чисел в массиве.
+// const numbers = [1, 10, 17, 24, 45]
+function average(arr){
+  let result = arr.reduce(function(sum, curr){
+    return (sum+curr)
+  })
+  return result/arr.length
+}
+console.log(average([1, 10, 17, 24, 45]))
+
+
+//Second task
+// 2.1
+  const logger = (element, index, array) => {
+  console.log(`In array [${array}] on position ${index}: ${element}`);
+  };
+  const ownForEach = function(arr, callBackFunction){
+      for (let i=0; i<arr.length; i++) {
+          callBackFunction(arr[i], i, arr)
+      }
+      
+  }
+  ownForEach([1, 2, 3, 7, 10, 12], logger);
+
+// 2.2
+  const increment = (number) => number + 1;
+  const ownMap = function(arr, callBackFunction){
+      for (let i=0; i<arr.length; i++) {
+          arr[i] = callBackFunction(arr[i]);
+      }
+      return arr
+  }
+  console.log(ownMap([1, 2, 3], increment)); // [2, 3, 4]
+
+//2.3
+  const isNegative = (number) => number < 0;
+  const ownFilter = function(arr, callBackFunction){
+      let arrNew =[]
+      for (let i=0; i<arr.length; i++){
+          if (callBackFunction(arr[i])) {
+              arrNew.push(arr[i])
+          }
+      }
+      return arrNew
+  }
+  console.log(ownFilter([-2, 4, -1], isNegative)); // [-2, -1]
+
+
+
+
+//Third task
 const data = [
     {
       name: "John",
       age: 24,
       position: "senior",
-      isActive: false,
+      isActive: true,
     },
     {
       name: "Peter",
-      age: 33,
+      age: 24,
       position: "middle",
-      isActive: false,
+      isActive: true,
     },
     {
       name: "Sam",
@@ -78,120 +168,12 @@ const data = [
     },
   ];   
   const filterData = (dataArr, obj) =>{
-    const keyArr = [];
-    const itemArr =[];
-    let newData =[];
-    let result = [];
-// поиск ключей? которые подходят под условия
-        for (let key in obj){
-          newData = dataArr.filter((item) => item[key] === obj[key])     
-          keyArr.push(key);  
-          itemArr.push(obj[key])
-        }
-//проверка, что все ключи совпадают 
-        newData.forEach((item, i) => {
-            for (let j=0; j<keyArr.length; j++){
-           if (item[keyArr[j]]===itemArr[j] && item[keyArr[i]]===itemArr[i]) {
-            result.push(item)
-           }
-        } 
-        })      
-// проверка дубликатов
-        return result.filter((it, index) => index === result.indexOf(it));     
-}
-    
-
-  console.log(filterData(data, { age: 23 }));
-  // [
-  //     {
-  //         "name": "Steve",
-  //         "age": 23,
-  //         "position": "middle",
-  //         "isActive": true
-  //     }
-  // ]
-
-  console.log(filterData(data, { age: 24 }));
-  // [
-  //     {
-  //         "name": "John",
-  //         "age": 24,
-  //         "position": "senior",
-  //         "isActive": false
-  //     },
-  //     {
-  //         "name": "Mary",
-  //         "age": 24,
-  //         "position": "middle",
-  //         "isActive": false
-  //     }
-  // ]
-  
-console.log(filterData(data, { age: 19, position: "junior" }));
-  // [
-  //     {
-  //         "name": "Sally",
-  //         "age": 19,
-  //         "position": "junior",
-  //         "isActive": false
-  //     }
-  // ]
-  // то есть, функция вернула массив со всеми работниками на позиции junior и всеми работниками, которым 19 лет
-
-  
-// 1.3
-// Дан массив чисел
-// С помощью метода reduce вычислите среднее арифметическое чисел в массиве.
-// const numbers = [1, 10, 17, 24, 45]
-
-function average(arr){
-  let result = arr.reduce(function(sum, curr){
-    return (sum+curr)
-  })
-  return result/arr.length
+    for (let key in obj){
+            dataArr = dataArr.filter((item) => item[key] === obj[key])     
+              }
+   return dataArr
 }
 
-console.log(average([1, 10, 17, 24, 45]))
-
-
-//Second task
-    const logger = (element, index, array) => {
-    console.log(`In array [${array}] on position ${index}: ${element}`);
-    };
-
-    const ownForEach = function(arr, callBackFunction){
-        for (let i=0; i<arr.length; i++) {
-            callBackFunction(arr[i], i, arr)
-        }
-        
-    }
-    ownForEach([1, 2, 3, 7, 10, 12], logger);
-
-
-    const increment = (number) => number + 1;
-    const ownMap = function(arr, callBackFunction){
-        let arrNew = [];
-        for (let i=0; i<arr.length; i++) {
-            arrNew[i] = callBackFunction(arr[i]);
-        }
-        return arrNew
-    }
-    console.log(ownMap([1, 2, 3], increment)); // [2, 3, 4]
-
-
-    const isNegative = (number) => number < 0;
-    const ownFilter = function(arr, callBackFunction){
-        let arrNew =[]
-        for (let i=0; i<arr.length; i++){
-            if (callBackFunction(arr[i])) {
-                arrNew.push(arr[i])
-            }
-        }
-        return arrNew
-    }
-    console.log(ownFilter([-2, 4, -1], isNegative)); // [-2, -1]
-
-
-
-
-   
+console.log(filterData(data, { age: 24, isActive: true }));
+console.log(filterData(data, { age: 19}));
+console.log(filterData(data, { position: 'middle', age: 24}));
