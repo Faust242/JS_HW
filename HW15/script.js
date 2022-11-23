9d69f3f8b52189d47af1037b29889dcfdd668647
Faust242 <-add>
2022-11-23 14:58:50 +0200

    hw15 done

--- HW15/script.js
+++ HW15/script.js
@@ -77,15 +77,53 @@ console.log(newMaxFunction(2,3,4,5,1,3,10,1,2,15,24,5,5,6,4,2,2))
 // Guest для поля author
 // текущую дату для поля time
 
-const createMessage = (obj) => {
-  return `From ${author} to ${reciever}: ${text} (${time.toLocaleDateString()})`;
-};
-const message = createMessage("Peter", "Hello", "Sam", new Date());
-
-console.log(message)
+const createMessage = ({ author = 'Guest', text, reciever, time = new Date()}) => {
+    return `From ${author} to ${reciever}: ${text} (${time.toLocaleDateString()})`;
+  };
+  
+//   const message = createMessage({author: "Peter", text: "Hello", reciever: "Sam", time: new Date()});
 // после выполнения этого задания, функция должна коректно работать с таким аргументом
-// const message = createMessage({
-//   reciever: "John",
-//   text: "Hi!",
-// });
+const message = createMessage({
+  reciever: "John",
+  text: "Hi!",
+});
+console.log(message)
 
+
+// Fifth task
+//5.1
+// Напишите регулярное выражение, которое находит подстроки с такими свойствами:
+// начинается с буквенного символа
+// заканчивает на буквенный символ
+// между первым и послденим символом находятя только числовые символы
+// для поиска используйте метод match
+let str = "x1y 722a 333 a123v1n a55555a qwe1 1zxc";
+// для строки str результат должен быть следующий: 
+// [ 'x1y', '722a', '333', 'a123v', 'a55555a' ]
+
+console.log(str.match(/\w[\d]+\w/g))    
+
+
+// 5.2Напишите регулярное выражение для валидации домена (адреса сайта)
+// Валидные домены: ex.ua, google.com, yandex.ru, site.com.ua, my-page.com
+// то есть, доменные имена начинаются с любого количества буквенных символов, чисел, символов - _ ., 
+// заканчиваются подстрокой, длина которой не менее 2 символов. Начало и конец обязательно разделены точкой
+// используйте метод test
+
+
+
+const testResult = /^[a-zA-Z0-9][a-zA-Z0-9-_]{1,}[a-zA-Z0-9]\.[a-zA-Z]{2,}$/g
+
+console.log(testResult.test('google.com'))
+console.log(testResult.test('ex.ua'))
+console.log(testResult.test('yandex.ru'))
+console.log(testResult.test('site.com.ua'))
+console.log(testResult.test('my-page.com'))
+console.log(testResult.test('s.com.ua'))
+console.log(testResult.test('my.c'))
+console.log(testResult.test('m_y.c0'))
+
+//5.3
+// Напишите регулярное выражаение, которое проверяет строку:
+// строка не должна содержать ничего кроме числовых символов
+// длина строка должна быть не менее 12, но можно и больше
